1ff9e9a9d5faf01d0ad3cad29933b111a22171c4
Kevin Tran <kevinat1@uci.edu>
2024-12-06 12:46:16 -0800

    affinda resume parsing wip

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const { AffindaCredential, AffindaAPI } = require("@affinda/affinda");
+const { affindaToken } = require("./affinda_token.js");
+const fs = require("fs");
+
 // port
 const port = 5001;
 
@@ -9,12 +13,29 @@ app.use(express.json());
 
 // routes
 
-// test server
+// GET Route: tests whether Express Server is running
 app.get("/test", async (req, res) => {
   res.status(200).json({
     message: "Request was successfully sent",
   });
 });
+
+const credential = new AffindaCredential(affindaToken);
+const client = new AffindaAPI(credential);
+
+const filePath = "/Users/kevintran/Downloads/Kevin_Tran_Resume.pdf";
+const readStream = fs.createReadStream(filePath);
+
+client
+  .createDocument({ file: readStream })
+  .then((result) => {
+    console.log("Returned data:");
+    console.dir(result);
+  })
+  .catch((err) => {
+    console.log("An error occurred:");
+    console.error(err);
+  });
 
 app.listen(port, () => {
   console.log(`Express Server listening on Port ${port}`);
